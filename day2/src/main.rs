f3a913ba394673edd4bd70c61300752f0ed469a8
Oliver Sun <oliversuun@gmail.com>
2022-12-03 18:48:56 -0500

    day 2 part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -20,41 +20,47 @@ fn main() {
                 break;
             }
         };
-        let own_shape = match line.chars().nth(2) {
-            Some(own_input) => {
-                match own_input {
-                    'X' => 0,
-                    'Y' => 1,
-                    'Z' => 2,
+        let expected_outcome = match line.chars().nth(2) {
+            Some(second_input) => {
+                match second_input {
+                    'X' => 2,
+                    'Y' => 0,
+                    'Z' => 1,
                     _ => {
-                        println!("read invalid own move: {}", own_input);
+                        println!("read invalid own outcome: {}", second_input);
                         break;
                     }
                 }
             }
             None => {
-                println!("none own move read");
+                println!("none own outcome read");
                 break;
             }
         };
-        // both moves are in {0, 1, 2}
-        res += get_shape_point(own_shape);
-        res += get_outcome_point(opponent_shape, own_shape)
+        res += match expected_outcome {
+            0 => 3,
+            1 => 6,
+            2 => 0,
+            _ => {
+                break;
+            }
+        };
+        res += (expected_outcome + opponent_shape)%3 + 1;
     }
     println!("result is {}", res)
 }
 
-fn get_shape_point(shape: i32) -> i32 {
-    shape + 1
-}
+// fn get_shape_point(shape: i32) -> i32 {
+//     shape + 1
+// }
 
-fn get_outcome_point(opponent_shape: i32,own_shape: i32) -> i32 {
-    match (own_shape - opponent_shape + 3)%3 {
-        0 => 3,
-        1 => 6,
-        2 => 0,
-        _ => {
-            panic!("impossible");
-        }
-    }
-}
+// fn get_outcome_point(opponent_shape: i32,own_shape: i32) -> i32 {
+//     match (own_shape - opponent_shape + 3)%3 {
+//         0 => 3,
+//         1 => 6,
+//         2 => 0,
+//         _ => {
+//             panic!("impossible");
+//         }
+//     }
+// }
