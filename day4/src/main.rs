0f6ac54332d591c4744ef81a6fa5a5971afe49e4
Oliver Sun <oliversuun@gmail.com>
2022-12-04 17:10:53 -0500

    day 4 part 2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -16,8 +16,11 @@ fn main() {
             Some(range) => get_bounds(String::from(range)),
             None => break,
         };
-        if contains_range(range_1, range_2) || contains_range(range_2, range_1) {
-            res += 1
+        // if contains_range(range_1, range_2) || contains_range(range_2, range_1) {
+        //     res += 1
+        // }
+        if ranges_overlap(range_1, range_2) {
+            res += 1;
         }
     }
     println!("result is {}", res)
@@ -40,6 +43,13 @@ fn get_bounds(s: String) -> (u32, u32) {
     (lower, higher)
 }
 
-fn contains_range(r1: (u32, u32), r2: (u32, u32)) -> bool {
-    return r1.0 <= r2.0 && r2.1 <= r1.1
+// fn contains_range(r1: (u32, u32), r2: (u32, u32)) -> bool {
+//     return r1.0 <= r2.0 && r2.1 <= r1.1
+// }
+
+fn ranges_overlap(r1: (u32, u32), r2: (u32, u32)) -> bool {
+    return !(
+        r1.1 < r2.0 ||
+        r2.1 < r1.0
+    )
 }
